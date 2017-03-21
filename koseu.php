9025cd08c4574f8d5fdc6d75b7097f507336019f
Charles Severance <csev@umich.edu>
2017-03-21 18:32:37 -0400

    Progress - Not pretty (yet)

--- koseu.php
+++ koseu.php
@@ -4,22 +4,55 @@
  * Koseu - An entire LMS in a single file + .htaccess
  */
 
+use \Tsugi\Core\LTIX;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\PhpBridgeSessionStorage;
+
+define('COOKIE_SESSION', true);
 require_once "tsugi/config.php";
 
+$app = new \Silex\Application();
+$app['tsugi'] = LTIX::session_start();
+$OUTPUT->buffer = true;
+
+$session = new Session(new PhpBridgeSessionStorage());
+$session->start();
+$app['session'] = $session;
+
+class TsugiPage {
+    public $view = '{{ tsugi.output.header | raw }}
+{% block head %}
+{% endblock %}
+{{ tsugi.output.bodyStart | raw }} 
+{{ tsugi.output.topNav() | raw }} 
+{% block content %}{% endblock %}
+{{ tsugi.output.footerStart() | raw }}
+{% block footer %}
+{% endblock %}
+{{ tsugi.output.footerEnd() | raw }}
+';
+}
+
 class myview {
-    public $view = 'Hello name = {{ name }} from the \myview class';
+    public $view = '
+{% extends "\TsugiPage" %}
+
+{% block content %}
+Hello name = {{ name }} from the \myview class
+{% endblock %}
+';
 }
 
 $loader = new \Koseu\Twig\Twig_Loader_Class();
-$app = new \Silex\Application();
+$app->tsugi = $LAUNCH;
 
 $app->register(new \Silex\Provider\TwigServiceProvider(), array(
     'twig.loader' => $loader
 ));
 
 $app->get('/class/{name}', function ($name) use ($app) {
-    echo("<pre>\n");
     return $app['twig']->render('\myview', array(
+        'tsugi' => $app['tsugi'],
         'name' => $name,
     ));
 });
