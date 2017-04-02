a3baaaf85a0572155dc0384c0b2734b02e61630c
Charles Severance <csev@umich.edu>
2017-04-02 11:38:35 -0400

    Switch to simpler template approach w/ namespaces

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+</head>
+<body>
+<ul>
+<li><a href="class/bob">class/bob</li>
+</ul>
--- koseu.php
+++ koseu.php
@@ -12,32 +12,8 @@ require_once "tsugi/config.php";
 $launch = LTIX::session_start();
 $app = new \Tsugi\Silex\Application($launch);
 
-class TsugiPage {
-    public $view = '{{ tsugi.output.header | raw }}
-{% block head %}
-{% endblock %}
-{{ tsugi.output.bodyStart | raw }} 
-{{ tsugi.output.topNav() | raw }} 
-{% block content %}{% endblock %}
-{{ tsugi.output.footerStart() | raw }}
-{% block footer %}
-{% endblock %}
-{{ tsugi.output.footerEnd() | raw }}
-';
-}
-
-class myview {
-    public $view = '
-{% extends "\TsugiPage" %}
-
-{% block content %}
-Hello name = {{ name }} from the \myview class
-{% endblock %}
-';
-}
-
 $app->get('/class/{name}', function ($name) use ($app) {
-    return $app['twig']->render('\myview', array(
+    return $app['twig']->render('page.twig', array(
         'tsugi' => $app['tsugi'],
         'name' => $name,
     ));
